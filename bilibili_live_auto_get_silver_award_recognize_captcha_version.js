fd298abcb028a7c0426d621fb069de4ff85e3ee7
Shin Feng <shin.f.kan@gmail.com>
2017-02-10 10:10:41 +0800

    Update the script for new Bilibili capture.

--- bilibili_live_auto_get_silver_award_recognize_captcha_version.js
+++ bilibili_live_auto_get_silver_award_recognize_captcha_version.js
@@ -7,8 +7,8 @@
  * 银瓜子领完自动停止发送请求。
  *
  * @author 記憶の中で未来の風
- * @version 0.0.6
- * @date 2016.10.17
+ * @version 0.0.7
+ * @date 2017.1.21
  *
  */
 
@@ -45,13 +45,13 @@ var number0 = new Array(
 	"1111111111111111",
 	"0111111111111110");
 var number1 = new Array(
+	"0001111",
 	"0011111",
 	"0011111",
 	"0011111",
 	"0111111",
 	"0111111",
 	"0111111",
-	"1111111",
 	"1111111",
 	"1111111",
 	"0011111",
@@ -76,7 +76,7 @@ var number1 = new Array(
 	"0011111",
 	"0011111",
 	"0011111");
-var number2_1 = new Array(
+var number2 = new Array(
 	"0111111111111110",
 	"1111111111111111",
 	"1111111111111111",
@@ -87,60 +87,28 @@ var number2_1 = new Array(
 	"1111100000011111",
 	"1111100000011111",
 	"1111100000011111",
-	"0000000000111111",
-	"0000000000111111",
+	"0000000000011111",
+	"0000000000111110",
 	"0000000001111110",
-	"0000000011111100",
-	"0000000011111100",
-	"0000000111111000",
-	"0000000111111000",
-	"0000001111110000",
-	"0000011111100000",
-	"0000011111100000",
-	"0000111111000000",
-	"0000111110000000",
-	"0001111110000000",
-	"0011111100000000",
-	"0011111000000000",
-	"0111111000000000",
-	"0111110000000000",
-	"1111111111111111",
-	"1111111111111111",
-	"1111111111111111",
-	"1111111111111111");
-var number2_2 = new Array(
-	"0111111111111110",
-	"1111111111111111",
-	"1111111111111111",
-	"1111111111111111",
-	"1111100000011111",
-	"1111100000011111",
-	"1111100000011111",
-	"1111100000011111",
-	"1111100000011111",
-	"1111100000011111",
-	"0000000000111111",
-	"0000000000111111",
-	"0000000001111110",
-	"0000000011111110",
-	"0000000011111100",
-	"0000000111111000",
+	"0000000001111100",
+	"0000000011111000",
+	"0000000011111000",
 	"0000000111110000",
 	"0000001111110000",
-	"0000011111100000",
-	"0000011111100000",
-	"0000111111000000",
+	"0000001111100000",
+	"0000011111000000",
+	"0000011111000000",
 	"0000111110000000",
-	"0001111110000000",
-	"0011111100000000",
+	"0001111100000000",
+	"0001111100000000",
 	"0011111000000000",
-	"0111111000000000",
+	"0011110000000000",
 	"0111110000000000",
 	"1111111111111111",
 	"1111111111111111",
 	"1111111111111111",
 	"1111111111111111");
-var number3_1 = new Array(
+var number3 = new Array(
 	"0111111111111110",
 	"1111111111111111",
 	"1111111111111111",
@@ -151,46 +119,14 @@ var number3_1 = new Array(
 	"1111100000011111",
 	"1111100000011111",
 	"1111100000011111",
-	"0000000000111111",
-	"0000000001111111",
-	"0000000011111110",
-	"0000001111111000",
-	"0000001111110000",
-	"0000001111111000",
-	"0000000011111110",
-	"0000000001111111",
-	"0000000000111111",
 	"0000000000011111",
-	"0000000000011111",
-	"1111100000011111",
-	"1111100000011111",
-	"1111100000011111",
-	"1111100000011111",
-	"1111100000011111",
-	"1111100000011111",
-	"1111111111111111",
-	"1111111111111111",
-	"1111111111111111",
-	"0111111111111110");
-var number3_2 = new Array(
-	"0111111111111110",
-	"1111111111111111",
-	"1111111111111111",
-	"1111111111111111",
-	"1111100000011111",
-	"1111100000011111",
-	"1111100000011111",
-	"1111100000011111",
-	"1111100000011111",
-	"1111100000011111",
-	"0000000000111111",
-	"0000000001111111",
-	"0000000011111110",
-	"0000001111111000",
+	"0000000001111110",
+	"0000000011111100",
+	"0000000111111000",
 	"0000001111110000",
-	"0000001111111100",
-	"0000000111111110",
-	"0000000001111111",
+	"0000000111111000",
+	"0000000011111100",
+	"0000000001111110",
 	"0000000000111111",
 	"0000000000011111",
 	"0000000000011111",
@@ -205,27 +141,27 @@ var number3_2 = new Array(
 	"1111111111111111",
 	"0111111111111110");
 var number4 = new Array(
-	"00000001111110000",
 	"00000001111100000",
-	"00000011111100000",
-	"00000011111100000",
+	"00000001111100000",
+	"00000001111100000",
 	"00000011111000000",
-	"00000111111000000",
-	"00000111111000000",
+	"00000011111000000",
+	"00000011111000000",
 	"00000111110000000",
-	"00001111110000000",
-	"00001111110000000",
+	"00000111110000000",
+	"00000111110000000",
 	"00001111100000000",
-	"00011111100000000",
-	"00011111101111100",
+	"00001111100000000",
+	"00001111100000000",
 	"00011111001111100",
-	"00111111001111100",
-	"00111111001111100",
+	"00011111001111100",
+	"00011111001111100",
 	"00111110001111100",
-	"01111110001111100",
-	"01111110001111100",
+	"00111110001111100",
+	"00111110001111100",
 	"01111100001111100",
-	"11111100001111100",
+	"01111100001111100",
+	"01111100001111100",
 	"11111111111111111",
 	"11111111111111111",
 	"11111111111111111",
@@ -300,69 +236,37 @@ var number6 = new Array(
 	"1111111111111111",
 	"1111111111111111",
 	"0111111111111110");
-var number7_1 = new Array(
+var number7 = new Array(
 	"1111111111111111",
 	"1111111111111111",
 	"1111111111111111",
 	"1111111111111111",
 	"1111100000011111",
-	"1111100000111111",
+	"1111100000011110",
 	"1111100000111110",
 	"1111100000111110",
 	"0000000000111110",
-	"0000000001111110",
+	"0000000000111100",
 	"0000000001111100",
 	"0000000001111100",
 	"0000000001111100",
-	"0000000011111100",
+	"0000000001111000",
 	"0000000011111000",
 	"0000000011111000",
-	"0000000111111000",
-	"0000000111111000",
-	"0000000111110000",
-	"0000000111110000",
-	"0000001111110000",
-	"0000001111110000",
-	"0000001111100000",
-	"0000001111100000",
-	"0000011111100000",
-	"0000011111100000",
-	"0000011111000000",
-	"0000011111000000",
-	"0000111111000000",
-	"0000111111000000",
-	"0000111110000000");
-var number7_2 = new Array(
-	"1111111111111111",
-	"1111111111111111",
-	"1111111111111111",
-	"1111111111111111",
-	"1111100000011111",
-	"1111100000111111",
-	"1111100000111110",
-	"1111100000111110",
-	"0000000000111110",
-	"0000000001111110",
-	"0000000001111100",
-	"0000000001111100",
-	"0000000011111100",
-	"0000000011111100",
 	"0000000011111000",
-	"0000000011111000",
-	"0000000111111000",
-	"0000000111111000",
+	"0000000011110000",
 	"0000000111110000",
 	"0000000111110000",
-	"0000001111110000",
-	"0000001111110000",
+	"0000000111110000",
 	"0000001111100000",
 	"0000001111100000",
-	"0000011111100000",
-	"0000011111100000",
+	"0000001111100000",
+	"0000001111100000",
 	"0000011111000000",
 	"0000011111000000",
-	"0000111111000000",
-	"0000111111000000",
+	"0000011111000000",
+	"0000011111000000",
+	"0000111110000000",
 	"0000111110000000");
 var number8 = new Array(
 	"0111111111111110",
@@ -376,13 +280,13 @@ var number8 = new Array(
 	"1111100000011111",
 	"1111100000011111",
 	"1111100000011111",
-	"1111110000111111",
-	"0111111111111110",
-	"0011111111111100",
+	"0111110000111111",
+	"0111111101111110",
 	"0001111111111000",
-	"0011111111111100",
+	"0000111111110000",
+	"0001111111111000",
 	"0111111111111110",
-	"1111111001111111",
+	"1111110000111111",
 	"1111100000011111",
 	"1111100000011111",
 	"1111100000011111",
@@ -493,8 +397,8 @@ var minus = new Array(
 	"000000000",
 	"000000000");
 var empty = new Array(" ");
-var captchaBinarySignature = new Array(number0, number1, number2_1, number3_1, number4, number5, number6, number7_1, number8, number9,
-	                                   plus, minus, number2_2, number3_2, empty, empty, empty, number7_2);
+var captchaBinarySignature = new Array(number0, number1, number2, number3, number4, number5, number6, number7, number8, number9,
+	                                   plus, minus);
 
 function canGetFreeSilverAward() {
 	var timer = document.querySelector(".count-down-node");
@@ -526,7 +430,7 @@ function getCaptchaImg() {
 	    var data = content.getImageData(0, 0, img.width, img.height).data;//读取整张图片的像素。
 	    // console.log(data, data.toString());
 	}
-	img.src = "http://live.bilibili.com/FreeSilver/getCaptcha?ts=" + new Date().getTime();
+	img.src = "http://live.bilibili.com/FreeSilver/getCaptcha?ts=" + (new Date().getTime());
 }
 
 function removeCaptchaImg() {
@@ -534,12 +438,14 @@ function removeCaptchaImg() {
 	canvasElement.parentNode.removeChild(canvasElement);
 }
 
+var imgWidth = 300;
+var imgHeight = 150
 function binarizeCaptchaImg() {
 	var content = canvas.getContext('2d');
-	var data = content.getImageData(0, 0, 120, 40).data;
+	var data = content.getImageData(0, 0, imgWidth, imgHeight).data;
 	var captchaImgBinary = "";
 	for (var rPixel = 0, length = data.length; rPixel < length; rPixel += 4) {
-	   	if (data[rPixel] > 207) {
+	   	if (data[rPixel] > 127) {
 	   		captchaImgBinary += "0";
 	   	} else {
 	   		captchaImgBinary += "1";
@@ -551,7 +457,7 @@ function binarizeCaptchaImg() {
 function logCaptchaImgBinary(binary) {
 	var formalizedBinaryLog = "";
 	for (var index = 0, length = binary.length; index < length; index++) {
-		if (index % 120 == 0) {
+		if (index % imgWidth == 0) {
 			formalizedBinaryLog += "\n";
 		}
 		formalizedBinaryLog += binary[index];
@@ -561,19 +467,19 @@ function logCaptchaImgBinary(binary) {
 }
 
 function recognizeCaptcha(binary) {
-	var captcha = "";
-	for (var index = 120 * 6; index < 120 * 7 - 20; index++) {
+	var recognized_captcha = "";
+	for (var index = imgWidth * 4; index < imgWidth * 5 - 200; index++) {
 		for (var indexOfCaptchaBinarySignature = captchaBinarySignature.length; indexOfCaptchaBinarySignature > 0; indexOfCaptchaBinarySignature--) {
 			for (var line = 0; line < captchaBinarySignature[indexOfCaptchaBinarySignature-1].length; ) {
-				if (binary.substr(index + 120 * line, captchaBinarySignature[indexOfCaptchaBinarySignature-1][line].length) == captchaBinarySignature[indexOfCaptchaBinarySignature-1][line]) {
+				if (binary.substr(index + imgWidth * line, captchaBinarySignature[indexOfCaptchaBinarySignature-1][line].length) == captchaBinarySignature[indexOfCaptchaBinarySignature-1][line]) {
 					line++;
 					if (line == captchaBinarySignature[indexOfCaptchaBinarySignature-1].length) {
 						if ((indexOfCaptchaBinarySignature-1) == 11) {
-							captcha += " - ";
+							recognized_captcha += " - ";
 						} else if ((indexOfCaptchaBinarySignature-1) == 10) {
-							captcha += " + ";
+							recognized_captcha += " + ";
 						} else {
-							captcha += (indexOfCaptchaBinarySignature - 1) % 10;
+							recognized_captcha += (indexOfCaptchaBinarySignature - 1) % 10;
 						}
 						
 					}
@@ -583,7 +489,7 @@ function recognizeCaptcha(binary) {
 			}
 		}		
 	}
-	return captcha;
+	return recognized_captcha;
 }
 
 function calculateCaptcha(captcha) {
@@ -638,28 +544,27 @@ function getFreeSilverAward() {
 		setTimeout(function () {
 			var binary = binarizeCaptchaImg();
 			var formalizedBinary = logCaptchaImgBinary(binary).replace(/\n/g, "");
-			var captcha = recognizeCaptcha(formalizedBinary);
-			var result = calculateCaptcha(captcha);
-			console.log("Captcha:"+captcha);
+			var recognized_captcha = recognizeCaptcha(formalizedBinary);
+			var result = calculateCaptcha(recognized_captcha);
+			console.log("Captcha:"+recognized_captcha);
 			console.log("Captcha Result:" + result);
 			// console.log(new Date(parseInt(new Date().getTime())).toLocaleString().replace(/:\d{1,2}$/,' '));
 			removeCaptchaImg();
 			inputCaptcha(result);
 			clickGetFreeSilverAwardBtn();
 			// clickCloseBtn();
-		}, 3000);
+		}, 5000);
 	}	
 }
 
 var timer;
 function autoGetFreeSilverAward() {
-	timer = window.setInterval(function() {getFreeSilverAward();}, 5000);
+	timer = window.setInterval(function() {getFreeSilverAward();}, 10000);
 	var clearTimer = window.setInterval(function () {
 		if (noMoreSilverAward()) {
 			clearInterval(timer);
 		}
-	}, 100);
+	}, 200);
 }
 
-var auto = autoGetFreeSilverAward();
-// getFreeSilverAward();
+autoGetFreeSilverAward();
